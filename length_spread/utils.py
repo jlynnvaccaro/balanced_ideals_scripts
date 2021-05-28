bd047b400fb48d845e1550344460055208f2bf57
Jennifer Vaccaro <jvacca4@uic.edu>
2021-05-28 16:00:28 -0500

    Fixed length_spred, added features to check if lower half ideals had the most generators

--- length_spread/utils.py
+++ length_spread/utils.py
@@ -1,8 +1,15 @@
 def get_ideal_stats(line):
-    L = line[28:].strip().split()
+    """Gets the max_length of the ideal and the number of generators"""
+    L = line.strip().split(":")
+    L = L[-1].strip().split()
     m = 0
     for gen in L:
         gen_l = len(gen)
         if gen_l>m:
             m = gen_l
     return m, len(L)
+
+def get_gen_set(line):
+    L = line.strip().split(":")
+    L = L[-1].strip().split()
+    return L
